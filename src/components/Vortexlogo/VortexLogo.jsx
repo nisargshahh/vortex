fdeba7f07b8b7e72fb2aa719da7fe1b69ee4ed37
Nisarg Shah <devnisargshah@gmail.com>
2022-11-13 10:44:42 +0530

    small updates

---
+++ src/components/Vortexlogo/VortexLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function VortexLogo() {
+  return (
+    <div className="logo-screen">
+      <img
+        src={require("../Images/Copy of Vortex.png")}
+        alt="Vortex Logo"
+        className="logo-screen-img"
+      />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/Navbar/NavBar";
 import Footer from "./components/Footer/Footer";
 import SideBar from "./components/Sidebar/SideBar";
+import VortexLogo from "./components/Vortexlogo/VortexLogo";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <NavBar />
     <SideBar/>
-    <Footer />
-    
+    <Footer />    
   </React.StrictMode>
 );
 
