29f28af034ecf61510752c3fc39fee10fb181a48
Nisarg Shah <devnisargshah@gmail.com>
2022-11-17 01:32:18 +0530

    feedback page

--- src/pages/Feedback/feedback.js
+++ src/pages/Feedback/feedback.js
@@ -2,13 +2,19 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "../../components/Navbar/NavBar";
 import Footer from "../../components/Footer/Footer";
+import './feedback.css'
 
 function feedback() {
   return (
     <div>
       <NavBar />
       <div className="feedback">
-        feedback
+        <h1 className="expt-heading" id="feefoo">
+          <u>Feedback Form</u>
+        </h1>
+        <div className="feedback-form">
+         <iframe src="https://forms.gle/nnFFtgzASkJ8wkU27" title="g-form" className="g-form"></iframe>
+        </div>
       </div>
       <Footer />
     </div>
