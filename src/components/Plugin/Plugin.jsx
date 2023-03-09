ddfb156c0ebc785c8dce1381478287de08cac645
Sheetal-12 <sheetal.popat@somaiya.edu>
2023-03-09 08:57:55 +0530

    Added code for the jdoodle compiler plugin

---
+++ src/components/Plugin/Plugin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Plugin() {
+  return (
+    <div>
+<div data-pym-src='https://www.jdoodle.com/plugin' data-language="python"
+    data-version-index="4" data-libs="mavenlib1, mavenlib2">
+    Code
+  </div>
+  <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"></script>
+    </div>
+  );
+}
