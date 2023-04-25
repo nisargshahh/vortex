aff2f1fb446d419f2f4e810c4fc423354ced80a3
Sheetal-12 <sheetal.popat@somaiya.edu>
2023-04-26 00:08:56 +0530

    Added simulation for experiment 2

---
+++ src/components/NetworkS/networks.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./networks.css";
+
+export default class NetworkS extends Component {
+    render() {
+      return (
+        <div >
+          <button
+            className="network-btn"
+            onClick={() => {
+
+            }}
+          >
+            {" "}
+            Go to simulation
+          </button>
+        </div>
+      );
+    }
+  }
+  
