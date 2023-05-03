376e0955a9cb0f58337e0770e410b7cfdd624c16
Hima Verma <79623293+hima-v@users.noreply.github.com>
2023-05-03 10:44:00 +0530

    markov simulation

--- src/components/Experiments/Expt6/Expt6.jsx
+++ src/components/Experiments/Expt6/Expt6.jsx
@@ -5,102 +5,34 @@ export default class Expt1 extends Component {
   render() {
     return (
       <div className="expt-template">
-        <h1 className="expt-heading">Study on Maximum Likelihood Estimation</h1>
+        <h1 className="expt-heading">6. Study on Maximum Likelihood Estimation</h1>
         <h2 className="expt-sub-heading">
-          <u>Introduction</u>
+          <u>Aim</u>
         </h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
-          aliquam. Reprehenderit repellendus tenetur cumque eius iusto adipisci
-          mollitia nam impedit reiciendis blanditiis voluptatibus, praesentium
-          accusamus magnam numquam fugit, debitis quo deserunt pariatur
-          explicabo doloremque? Ducimus et neque inventore, omnis dignissimos
-          veniam accusantium rem animi sed autem porro quo. Perspiciatis omnis
-          ipsa quasi voluptate nesciunt voluptatem repellendus ad exercitationem
-          molestias dolorum dolores, praesentium adipisci saepe minima veniam
-          harum asperiores ipsam ea aliquid et temporibus cum atque obcaecati.
-          Doloribus commodi excepturi consequuntur, similique magni atque
-          impedit harum odio, quis amet omnis? Perferendis impedit harum fuga
-          totam accusantium illum iusto, temporibus labore animi?
+          This experiment aims to study the statistical method called Maximum Likelihood Estimation
         </p>
 
         <h3 className="expt-sub-heading">
-          <u>Acknowlegdements</u>
+          <u>Theory</u>
         </h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
-          laudantium iure nostrum aliquid ullam voluptates impedit corrupti
-          reiciendis dolores quis sed nihil, deleniti natus qui nemo! Velit
-          earum maiores in harum, sit ipsum? Voluptatem, quibusdam minus vero
-          recusandae sunt distinctio obcaecati aut voluptatum culpa nemo amet
-          non, incidunt illum placeat modi! Quaerat voluptate ipsam quod autem,
-          ipsum aut non aspernatur error voluptatibus illum eveniet nemo. Saepe
-          autem excepturi deserunt soluta, magnam sint totam rem commodi error
-          laudantium perferendis corrupti incidunt?
+          Maximum likelihood estimation (MLE) is a statistical method used to estimate the parameters of a probability distribution, given a set of observed data. The basic idea behind MLE is to find the parameter values that maximize the likelihood function, which is a function of the observed data and the model parameters. <br></br>
+          In other words, MLE seeks to find the parameter values that make the observed data most likely, according to the assumed probability distribution. This is done by calculating the likelihood of the observed data under different parameter values, and then choosing the parameter values that give the highest likelihood. <br></br>
+          The MLE method is widely used in various fields, including finance, economics, biology, and engineering, among others. It is a popular and powerful tool for estimating parameters in statistical models, especially in cases where the probability distribution is unknown or complex. <br></br>
+          Maximum likelihood estimation (MLE) is a very important method in statistical inference because it provides a systematic way to estimate the parameters of a statistical model. Some key benefits of using MLE include: <br></br>
+          1. Efficiency: MLE is known to be an efficient method, meaning that it produces estimates that are close to the true parameter values with a relatively small sample size. This makes it a useful tool for making inferences about the population parameters, even with limited data. <br></br>
+          2. Unbiasedness: In many cases, MLE produces unbiased estimates of the true parameter values. This means that the expected value of the estimate is equal to the true parameter value, which is an important property for statistical inference. <br></br>
+          3. Flexibility: MLE can be applied to a wide range of statistical models, including both simple and complex models. It is also possible to use MLE to estimate the parameters of non-standard distributions, which can be useful in many real-world applications. <br></br>
+          4. Consistency: MLE produces estimates that become more accurate as the sample size increases. This is known as consistency and is a desirable property for any statistical method. <br></br>
         </p>
 
         <h3 className="expt-sub-heading">
-          <u>Features</u>
-        </h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda,
-          ratione repellendus accusantium earum dignissimos tempore id quam
-          ducimus magni consectetur harum, corrupti pariatur optio nobis qui quo
-          deserunt numquam quasi:
-        </p>
-        <ul className="lab-manual">
-          <li className="lab-manual-heading">Lab Objectives</li>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum
-            distinctio ex quasi minus magni nobis sit fugit ab eius quo!
-            <ul>
-              <li>1. Lorem ipsum dolor sit amet consectetur.</li>
-              <li>2. Lorem ipsum dolor sit amet consectetur.</li>
-              <li>3. Lorem ipsum dolor sit amet consectetur.</li>
-              <li>4. Lorem ipsum dolor sit amet consectetur.</li>
-            </ul>
-          </p>
-          <li className="lab-manual-heading">Materials Required</li>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
-            minima laudantium rem est? Quaerat suscipit quis culpa illo, officia
-            doloribus.
-          </p>
-          <li className="lab-manual-heading">Completion Times</li>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Accusantium earum a nisi facilis ipsa libero accusamus dolore culpa
-            aspernatur esse?
-          </p>
-          <li className="lab-manual-heading">Activity Sections</li>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
-            alias consectetur, consequatur perferendis ipsum temporibus illo at!
-            Earum, accusantium iste.
-          </p>
-          <li className="lab-manual-heading">Step-by-Step Instructions</li>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam eos
-            corporis perspiciatis id iste corrupti accusamus cupiditate
-            blanditiis culpa repellendus?
-          </p>
-          <li className="lab-manual-heading">Review Questions</li>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequuntur molestias ducimus perferendis veniam alias fugiat
-            quisquam officiis voluptatum, quas debitis.
-          </p>
-        </ul>
-
-        <h3 className="expt-sub-heading">
-          <u>Questions</u>
+          <u>Simulation Links</u>
         </h3>
         <ol>
-          <li>What is the operating system of the computer you use?</li>
-          <li>How much main memory does the computer you use have?</li>
-          <li>What output devices areconnected to the computer you use? </li>
-          <li>How much secondary storage does the computer you use have?</li>
-          <li>When and where was ENIAC built?</li>
+          <li> <a href="https://colab.research.google.com/drive/1hWQTtBACd_vo9xW1-VKmtIw0OZTVE9kd?usp=sharing" > Basic understanding of MLE </a> </li>
+          <li> <a href="https://colab.research.google.com/drive/1jqPyyXo4vm8EyrIpDi8da8i8m9zV8hIr#scrollTo=takqBaX6S2Ho"> Code for implementing MLE </a> </li>
         </ol>
       </div>
     );
