b2d926e6e08f068a443ce42764f00a664b8ba4e0
Hima Verma <79623293+hima-v@users.noreply.github.com>
2023-06-28 10:25:39 +0530

    Change the UI to the Somaiya theme (red) Change the UI colour from blue to red theme Add the somaiya logo and department details in the footer.

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./landingpage.css";
 import "../../App"
+import bgimg from "../../components/Images/bgimg.jpeg"
 
 export default class LandingPage extends Component {
   render() {
@@ -9,39 +10,39 @@ export default class LandingPage extends Component {
       <div className="main">
         <div className="heading">
           <h1 className="vortex-name">Vortex</h1>
-          <h3 className="the-pgm-simulator">The PGM simulator</h3>
+          <h3 className="the-pgm-simulator">The PGM Simulator</h3>
           <h3 className="downward-arrow"><a href="#functions">🢓</a></h3>
         </div>
 
-          <div className="list-of-functs" id="functions">
-            <ul>
-              <li>
-                <Link to="/about-pgm">About PGM</Link>
-              </li>
-              <li>
-                <Link to="/module-obj">Module Objectives</Link>
-              </li>
-              <li>
-                <Link to="/lo-expts">List of Experiments</Link>
-              </li>
-              <li>
-                <Link to="/">Simulator</Link>
-              </li>
-              <li>
-                <Link to="/bayes-server">Bayes Server</Link>
-              </li>
-              <li>
-                <Link to="/papers">Papers</Link>
-              </li>
-              <li>
-                <Link to="/feedback">Feedback</Link>
-              </li>
-              <li>
-                <Link to="/projects">Projects</Link>
-              </li>
-            </ul>
-          </div>
+        <div className="list-of-functs" id="functions">
+          <ul>
+            <li>
+              <Link to="/about-pgm">About PGM</Link>
+            </li>
+            <li>
+              <Link to="/module-obj">Module Objectives</Link>
+            </li>
+            <li>
+              <Link to="/lo-expts">List of Experiments</Link>
+            </li>
+            <li>
+              <Link to="/">Simulator</Link>
+            </li>
+            <li>
+              <Link to="/bayes-server">Bayes Server</Link>
+            </li>
+            <li>
+              <Link to="/papers">Papers</Link>
+            </li>
+            <li>
+              <Link to="/feedback">Feedback</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
         </div>
+      </div>
     );
   }
 }
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -14,36 +14,36 @@ export default function navbar() {
             className="nav-logo-img"
           /></Nav.Link>
         </Navbar.Brand>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
         <NavbarCollapse>
-        <Nav className="navbar-list">
-          <Nav.Link className="nav-link" href="/">
-            Home
-          </Nav.Link>
-          <Nav.Link className="nav-link" href="/about-pgm">
-            About
-          </Nav.Link>
-          <Nav.Link className="nav-link" href="/feedback">
-            Contact
-          </Nav.Link>
-          <Nav.Link className="nav-link" href="/module-obj">
-            Other
-          </Nav.Link>
-          <NavDropdown title="Experiments">
-            <NavDropdown.Item href="/lo-expts/expt1">Expt 1</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt2">Expt 2</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt3">Expt 3</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt4">Expt 4</NavDropdown.Item> 
-            <NavDropdown.Item href="/lo-expts/expt5">Expt 5</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt6">Expt 6</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt7">Expt 7</NavDropdown.Item>
-            <NavDropdown.Item href="/lo-expts/expt8">Expt 8</NavDropdown.Item>
-          </NavDropdown>
-          
-        </Nav>
+          <Nav className="navbar-list">
+            <Nav.Link className="nav-link" href="/">
+              <b>Home</b>
+            </Nav.Link>
+            <Nav.Link className="nav-link" href="/about-pgm">
+              <b>About</b>
+            </Nav.Link>
+            <Nav.Link className="nav-link" href="/feedback">
+              <b>Contact</b>
+            </Nav.Link>
+            <Nav.Link className="nav-link" href="/module-obj">
+              <b>Other</b>
+            </Nav.Link>
+            <NavDropdown title=<b>Experiments</b>>
+              <NavDropdown.Item href="/lo-expts/expt1">Expt 1</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt2">Expt 2</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt3">Expt 3</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt4">Expt 4</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt5">Expt 5</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt6">Expt 6</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt7">Expt 7</NavDropdown.Item>
+              <NavDropdown.Item href="/lo-expts/expt8">Expt 8</NavDropdown.Item>
+            </NavDropdown>
+
+          </Nav>
         </NavbarCollapse>
       </Navbar>
-        
+
     </div>
   );
 }
